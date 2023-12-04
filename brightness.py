8ab7f0514c6d8224cc95afbe79a8c94815d60d64
familypet <36436403+familypet@users.noreply.github.com>
2023-12-04 20:10:42 +1100

    Update brightness.py
    Before prompting the user to enter a new brightness value, the script now iterates through each connected monitor and prints its current brightness level.
    
    The set_external_brightness function also prints the current brightness before setting it to the new value, providing a before-and-after comparison for each monitor.

--- brightness.py
+++ brightness.py
@@ -5,16 +5,23 @@ def set_external_brightness(value):
     monitors = sbc.list_monitors_info()
 
     for monitor in monitors:
-        if monitor['name'] != 'Laptop screen':
-            try:
-                sbc.set_brightness(value, display=monitor['serial'])
-                print(f"Brightness set to {value} for monitor {monitor['name']}")
-            except Exception as e:
-                print(f"Error setting brightness for monitor {monitor['name']}: {e}")
+        try:
+            current_brightness = sbc.get_brightness(display=monitor['serial'])
+            print(f"Current brightness for monitor {monitor['name']}: {current_brightness}%")
+            sbc.set_brightness(value, display=monitor['serial'])
+            print(f"Brightness set to {value}% for monitor {monitor['name']}")
+        except Exception as e:
+            print(f"Error setting brightness for monitor {monitor['name']}: {e}")
 
 if __name__ == "__main__":
     try:
-        brightness_value = int(input("Enter brightness value (0-100): "))
+        monitors = sbc.list_monitors_info()
+        for monitor in monitors:
+            current_brightness = sbc.get_brightness(display=monitor['serial'])
+            print(f"Current brightness for monitor {monitor['name']}: {current_brightness}%")
+        
+        brightness_value = int(input("Enter new brightness value (0-100): "))
+        
         if 0 <= brightness_value <= 100:
             set_external_brightness(brightness_value)
         else:
